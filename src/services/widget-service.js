177cc9ea709b4a364d64d1327494295b3202e75b
miaaaz <zhao.mia@northeastern.edu>
2021-03-21 13:41:31 +0800

    Fixed bugs

--- src/services/widget-service.js
+++ src/services/widget-service.js
@@ -1,7 +1,7 @@
-const LOCAL_HOST = process.env.REACT_APP_WIDGET_URL
+const WIDGET_URL = process.env.REACT_APP_WIDGET_URL
 
 export const createWidget = (tid, widget) =>
-    fetch(`${LOCAL_HOST}/topics/${tid}/widgets`, {
+    fetch(`${WIDGET_URL}/topics/${tid}/widgets`, {
       method: "POST",
       body: JSON.stringify(widget),
       headers: {
@@ -11,11 +11,11 @@ export const createWidget = (tid, widget) =>
     .then(response => response.json())
 
 export const findWidgetsForTopic = (tid) =>
-    fetch(`${LOCAL_HOST}/topics/${tid}/widgets`).then(res => res.json())
+    fetch(`${WIDGET_URL}/topics/${tid}/widgets`).then(res => res.json())
 
 
 export const updateWidget = (wid, widget) =>
-    fetch(`${LOCAL_HOST}/widgets/${wid}`,
+    fetch(`${WIDGET_URL}/widgets/${wid}`,
         {
           method: 'PUT',
           body: JSON.stringify(widget),
@@ -24,7 +24,7 @@ export const updateWidget = (wid, widget) =>
     ).then(res => res.json())
 
 export const deleteWidget = (wid) =>
-    fetch(`${LOCAL_HOST}/widgets/${wid}`,
+    fetch(`${WIDGET_URL}/widgets/${wid}`,
         {
           method: 'DELETE',
         }).then(res => res.json())
