9b3620b446c6ca6c3abbacc64ac8705411e14ece
miaaaz <zhao.mia@northeastern.edu>
2021-04-17 23:02:02 +0800

    Change configuration

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,4 +1,5 @@
-const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
+const QUIZZES_URL = process.env.REACT_APP_QUIZ_URL
+// const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
 
 const findQuestionsForQuiz = (qid) => {
   return fetch(`${QUIZZES_URL}/${qid}/questions`)
