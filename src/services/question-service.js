2e4b1271adc720a782e5c93fe46c2c68c63b4903
miaaaz <zhao.mia@northeastern.edu>
2021-04-17 23:51:03 +0800

    bugs fixed

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,4 +1,5 @@
-const QUIZZES_URL = process.env.REACT_APP_QUIZ_URL;
+// const QUIZZES_URL = "https://wbdv-sp21-mian-server-node.herokuapp.com/api/quizzes";
+const QUIZZES_URL = process.env.REACT_APP_QUIZ_URL
 
 const findQuestionsForQuiz = (qid) => {
   return fetch(`${QUIZZES_URL}/${qid}/questions`)
