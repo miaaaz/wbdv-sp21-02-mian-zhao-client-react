1cd85ad9feb8075f766a3aaf98bf9e50e3ffca43
miaaaz <zhao.mia@northeastern.edu>
2021-04-17 23:06:05 +0800

    Revert "Change configuration"
    This reverts commit 9b3620b446c6ca6c3abbacc64ac8705411e14ece.

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,5 +1,4 @@
-const QUIZZES_URL = process.env.REACT_APP_QUIZ_URL
-// const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
+const QUIZZES_URL = 'http://localhost:4000/api/quizzes';
 
 const findQuestionsForQuiz = (qid) => {
   return fetch(`${QUIZZES_URL}/${qid}/questions`)
